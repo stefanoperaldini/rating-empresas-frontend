09bc6f9d711392a1fc46b04034ffc262e4e05af9
stefanoperaldini <stefano.peraldini@gmail.com>
2020-01-21 17:14:01 +0100

    Internacionalizacion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import i18n from "i18next";
 
 import { Link } from "react-router-dom";
 
 export function Header() {
+  const [lang, setLang] = useState("en");
+
+  useEffect(() => {
+    const langStored = localStorage.getItem("re:lang");
+    if (langStored) {
+      console.log("Language stored", langStored)
+      setLang(langStored);
+      i18n.changeLanguage(langStored);
+    } else {
+      let userLang = navigator.language || navigator.userLanguage;
+      if (userLang) {
+        userLang = userLang.substring(0, 2);
+        switch (userLang) {
+          case "es":
+          case "it":
+          case "en":
+            i18n.changeLanguage(userLang);
+            setLang(userLang);
+            console.log("Language browser", userLang);
+            break;
+          default:
+            i18n.changeLanguage("en");
+            setLang(userLang);
+            console.log("Language browser no encontrado", userLang);
+            break;
+        }
+      } else {
+        i18n.changeLanguage("en");
+        setLang(userLang);
+        console.log("Default languade EN!!!!!");
+      }
+    }
+  }, [lang]);
+
+  console.log("Lenguage impostado", i18n.languages[0])
+
   return (
     <header className="header">
       <h1>Rating Empresas</h1>
+      <fieldset>
+        <ul>
+          <li>
+            <label>
+              English
+              <input
+                type="radio"
+                name="language"
+                value="en"
+                checked={lang === "en"}
+                onChange={e => {
+                  localStorage.setItem("re:lang", "en");
+                  setLang("en");
+                  i18n.changeLanguage("en");
+                }}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Spanish
+              <input
+                type="radio"
+                name="language"
+                value="es"
+                checked={lang === "es"}
+                onChange={e => {
+                  localStorage.setItem("re:lang", "es");
+                  setLang("es");
+                  i18n.changeLanguage("es");
+                }}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Italian
+              <input
+                type="radio"
+                name="language"
+                value="it"
+                checked={lang === "it"}
+                onChange={e => {
+                  localStorage.setItem("re:lang", "it");
+                  setLang("it");
+                  i18n.changeLanguage("it");
+                }}
+              />
+            </label>
+          </li>
+        </ul>
+      </fieldset>
       <nav>
         <ul>
           <li>
--- src/pages/ReviewCreate.js
+++ src/pages/ReviewCreate.js
@@ -1,92 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import i18n from "i18next";
 
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-
 import { Cities } from "../components/Cities";
 
+/**
+ * Review create
+ */
+
 export function ReviewCreate() {
-  const [lang, setLang] = useState("en");
 
-  useEffect(() => {
-    const langStored = localStorage.getItem("re:lang");
-    if (langStored) {
-      setLang(langStored);
-    } else {
-      let userLang = navigator.language || navigator.userLanguage;
-      if (userLang) {
-        userLang = userLang.substring(0, 2);
-        switch (userLang) {
-          case "es":
-          case "it":
-          case "en":
-            setLang(userLang);
-            break;
-          default:
-            setLang("en");
-            break;
-        }
-      } else {
-        setLang("en");
-      }
-    }
-  }, []);
-
-  i18n.changeLanguage(lang);
   return (
     <React.Fragment>
       <Header />
       <main>
-        <fieldset>
-          <ul>
-            <li>
-              <label>
-                English
-              <input
-                  type="radio"
-                  name="language"
-                  value="en"
-                  checked={lang === "en"}
-                  onChange={e => {
-                    setLang(e.target.value);
-                    localStorage.setItem("re:lang", "en");
-                  }}
-                />
-              </label>
-            </li>
-            <li>
-              <label>
-                Spanish
-              <input
-                  type="radio"
-                  name="language"
-                  value="es"
-                  checked={lang === "es"}
-                  onChange={e => {
-                    setLang(e.target.value);
-                    localStorage.setItem("re:lang", "es");
-                  }}
-                />
-              </label>
-            </li>
-            <li>
-              <label>
-                Italian
-              <input
-                  type="radio"
-                  name="language"
-                  value="it"
-                  checked={lang === "it"}
-                  onChange={e => {
-                    setLang(e.target.value);
-                    localStorage.setItem("re:lang", "it");
-                  }}
-                />
-              </label>
-            </li>
-          </ul>
-        </fieldset>
+        <h2>{i18n.t("This is the review create")}</h2>
         <Cities />
       </main>
       <Footer />
