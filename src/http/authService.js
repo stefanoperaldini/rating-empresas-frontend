a16db2f96e311a1161122487d10466e92c55598a
stefanoperaldini <stefano.peraldini@gmail.com>
2020-01-24 10:23:26 +0100

    Genstion enviroment

--- src/http/authService.js
+++ src/http/authService.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export function signIn(loginData) {
-    //return axios.post(`${process.env.REACT_APP_BACKEND_URL}/v1/accounts/login`, loginData);
-    return axios.post(`http://localhost:8000/v1/accounts/login`, loginData);
+    return axios.post(`${process.env.REACT_APP_BACKEND_URL}/v1/accounts/login`, loginData);
 }
